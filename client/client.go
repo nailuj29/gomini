affe6daf285875c7b7cfabe0d5a7e45033e75ece
Nailuj <nailuj29gaming@gmail.com>
2024-05-16 21:09:50 -0400

    Gemini Request function

--- client/client.go
+++ client/client.go
@@ -1,14 +1,68 @@
 package client
 
+import (
+	"crypto/tls"
+	log "github.com/sirupsen/logrus"
+	"io"
+	"net"
+	"net/url"
+	"strconv"
+	"strings"
+)
+
 // Response represents a Gemini Response
 type Response struct {
 	// Data contains the raw data returned from the server.
 	// TODO: implement function to pipe this straight to Gemtext parser
 	Data []byte
-	// ContentType contains the MIME type of the Data
-	ContentType string
+	// MetaData contains the metadata of the response.
+	// If StatusCode == 20, it is the MIME type associated with the data
+	MetaData string
+	// StatusCode contains the status code returned by the server.
+	// If this is not 20, Data should be considered to be empty
+	StatusCode int
 }
 
-func Request(url string) Response {
-	return Response{}
+func Request(address string, tlsConfig *tls.Config) (*Response, error) {
+	parsedURL, err := url.Parse(address)
+	if err != nil {
+		return nil, err
+	}
+	connInsecure, err := net.Dial("tcp", parsedURL.Host+":1965")
+	if err != nil {
+		return nil, err
+	}
+
+	conn := tls.Client(connInsecure, tlsConfig)
+
+	defer func(conn *tls.Conn) {
+		err := conn.Close()
+		if err != nil {
+			log.Errorf("%v", err)
+		}
+	}(conn)
+
+	_, err = conn.Write([]byte(address + "\r\n"))
+	if err != nil {
+		return nil, err
+	}
+
+	responseData, err := io.ReadAll(conn)
+	if err != nil {
+		return nil, err
+	}
+
+	header := strings.Split(string(responseData), "\r\n")[0]
+	headerParts := strings.Split(header, " ")
+	statusCode, err := strconv.Atoi(headerParts[0])
+	if err != nil {
+		return nil, err
+	}
+	metaData := strings.Join(headerParts[1:], " ")
+
+	return &Response{
+		StatusCode: statusCode,
+		Data:       responseData[len(header):],
+		MetaData:   metaData,
+	}, nil
 }
