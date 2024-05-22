d0524f34a9365b7d160a13752f4bcf47878e4f57
Nailuj <nailuj29gaming@gmail.com>
2024-05-22 00:03:19 -0400

    Add test for dynamic paths

--- client/client_server_test.go
+++ client/client_server_test.go
@@ -62,3 +62,56 @@ func TestBasicRequestResponse(t *testing.T) {
 		t.Fatalf("First line text is %s", headerLine.Text)
 	}
 }
+
+func TestDynamicPathRequestResponse(t *testing.T) {
+	cer, err := tls.LoadX509KeyPair("../examples/cert.pem", "../examples/key.pem")
+	if err != nil {
+		t.Fatal(err)
+	}
+	config := tls.Config{Certificates: []tls.Certificate{cer}, ClientAuth: tls.RequestClientCert}
+
+	s := server.New()
+
+	s.RegisterHandler("/:param", func(r server.Request) {
+		err := r.Gemtext(r.Params["param"])
+		if err != nil {
+			t.Errorf("handler failed to respond to request: %v", err)
+		}
+	})
+
+	go func() {
+		err := s.ListenAndServe("localhost", &config)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+	}()
+
+	clientConfig := tls.Config{InsecureSkipVerify: true}
+	response, err := Request("gemini://localhost/foo-bar", &clientConfig)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if response.StatusCode != 20 {
+		t.Fatalf("Response status code is %d", response.StatusCode)
+	}
+
+	if response.MetaData != "text/gemini" {
+		t.Fatalf("Response meta data is %s", response.MetaData)
+	}
+
+	parsed, err := gemtext.Parse(string(response.Data))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	textLine, ok := parsed[0].(gemtext.TextLine)
+	if !ok {
+		t.Fatalf("First line not a text line. Response data = %s", string(response.Data))
+	}
+
+	if textLine.Text != "foo-bar" {
+		t.Fatalf("First line text is %s", textLine.Text)
+	}
+}
