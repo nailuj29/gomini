7ac71cb7c7ce7477d803328597a00deaac54d0de
Nailuj <nailuj29gaming@gmail.com>
2024-05-22 15:57:52 -0400

    Remove TitanResponse struct. Replace with TitanRequest func

--- client/titan.go
+++ client/titan.go
@@ -1,7 +1,10 @@
 package client
 
-// A TitanResponse represents a Titan response
-type TitanResponse struct {
-	Response
-	Body []byte
+// TitanRequest sends a request to a Titan server
+//
+// If token or mime is not desired, an empty string can be passed
+func TitanRequest(address string, body []byte, token string, mime string) Response {
+	if mime == "" {
+		mime = "text/gemini"
+	}
 }
